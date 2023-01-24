a97664334ef167146e9a79256b8cad9e79929f2e
astinamuchui <sebastianmuchui79@gmail.com>
2023-01-24 15:34:05 +0300

    added

---
+++ _printf.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include <string.h>
+
+
+    /*
+        @params: format, unlimited number of characters to be printed
+    */
+
+int _printf ( const char *format , ...  )
+{
+    va_list args;
+    int i;
+
+    va_start ( args, format );
+    i = vprintf ( format , args );
+
+    va_end(args);
+
+    return i;
+}
+
+
+int main( void )
+{
+    _printf ("Hello, World!\n");
+}
