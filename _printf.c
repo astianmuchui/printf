06aba3d1c3fd8ac2ede008c5ccc8055fccaa2645
astianmuchui <sebastianmuchui79@gmail.com>
2023-01-24 15:55:51 +0300

    trying

--- _printf.c
+++ _printf.c
@@ -1,44 +1,28 @@
-#include "main.h"
 
-int _printf(const char *format, ...)
+#include <stdio.h>
+#include <stdarg.h>
+#include <string.h>
+
+
+    /*
+        @params: format, unlimited number of characters to be printed
+    */
+
+int _printf ( const char *format , ...  )
 {
-	int i = 0, j = 0, chars_printed = 0, flag = 0;
-	va_list arg_ptr;
-	char_t opt[] = {{"c", print_c}, {"s", print_s}, {"d", print_d},
-			{"i", print_i}, {"b", print_b}, {"u", print_u},
-			{"o", print_o}, {"x", print_x}, {"X", print_X},
-			{"S", print_S}, {NULL, NULL}};
+    va_list args;
+    int i;
 
-	va_start(arg_ptr, format);
+    va_start ( args, format );
+    i = vprintf ( format , args );
 
-	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
-		return (-1);
+    va_end(args);
 
-	while (format != NULL && format[i] != '\0') 
-	{
-		if (format[i] == '%' && format[i + 1] == '%')
-			_putchar(format[i + 1]), i++, chars_printed++;
-		else if (format[i] == '%' && format[i + 1] != '%')
-		{
-			j = 0, flag = 0;
-			while (opt[j].code != NULL)
-			{
-				if (opt[j].code[0] == format[i + 1])
-				{
-					chars_printed += opt[j].print_func(arg_ptr), flag = 1, i++;
-					break;
-				}
-				j++;
-			}
+    return i;
+}
 
-			if (!flag)
-				_putchar(format[i]), chars_printed++;
-		}
 
-		else
-			_putchar(format[i]), chars_printed++;
-		i++;
-	}
-	va_end(arg_ptr);
-	return (chars_printed);
+int main( void )
+{
+    // _printf ( "%d",500 );
 }
